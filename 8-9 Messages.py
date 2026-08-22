c6be3265c42a469ed8da67b8da449288891e2e71
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-22 10:51:19 -0400

    Add show_messages function to display messages

---
+++ 8-9 Messages.py
@@ -0,0 +1,12 @@
+def show_messages(messages):
+    for message in messages:
+        msg = f"Message Says: {message}"
+        print(msg)
+
+
+
+
+
+
+short_messages = ['Hi!', 'Good Morning!', 'What Doing?', '67']
+show_messages(short_messages)
